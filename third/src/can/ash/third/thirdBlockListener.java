e96e462ed8195c66f2fd0d83a3e77bb983f02c91
Ash88 <ash@hartwick.ca>
2011-04-09 09:41:32 -0400

    first init

---
+++ third/src/can/ash/third/thirdBlockListener.java
@@ -0,0 +1,27 @@
+﻿package can.ash.third;
+
+import org.bukkit.block.*;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.BlockCanBuildEvent;
+import org.bukkit.event.block.BlockListener;
+import org.bukkit.event.block.BlockPhysicsEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.*;
+import org.bukkit.inventory.Inventory;
+
+/**
+* third block listener
+* @author Arius_james
+*/
+public class thirdBlockListener extends BlockListener {
+private final third plugin;
+public String stuff ="""";
+
+public thirdBlockListener(final third plugin) {
+this.plugin = plugin;
+}
+
+
+
+} 
---
+++ third/src/can/ash/third/thirdPlayerListener.java
@@ -0,0 +1,40 @@
+﻿package com.bukkit.Arius_james.third;
+
+import java.io.*;
+import org.bukkit.Location;
+import org.bukkit.entity.*;
+import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.event.player.PlayerListener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.*;
+import org.bukkit.material.MaterialData;
+import org.bukkit.*;
+import org.bukkit.ChatColor;
+
+
+
+
+
+/**
+* Handle events for all Player related events
+* @author Arius_james
+*/
+public class thirdPlayerListener extends PlayerListener {
+private final third plugin;
+
+public thirdPlayerListener(third instance) {
+plugin = instance;
+}
+
+public void onPlayerCommand(PlayerChatEvent event) {
+Player players = event.getPlayer();
+String[] message = event.getMessage().split("" "");
+if(message[0].equalsIgnoreCase(""/clearinventory"")) {
+players.getInventory().clear();
+}
+}
+
+
+
+}
